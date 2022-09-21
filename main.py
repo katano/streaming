dd74bde49b82847187700e0dc574af45e4741423
tajmahal <tajmahal@nifty.com>
2022-09-21 16:43:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import numpy as np
 import streamlit as st
+import cv2
+import numpy as np
+import requests
 
+# Title of the app
+st.title( 'Mobile Camera Preview in Streamlit' )
 
+# An empty image container that will hold the frames we'll fetch from the server
+frame_window = st.image( [] )
