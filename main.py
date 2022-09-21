212bcd1271b968e73d6f48957c794f232d19aa51
tajmahal <tajmahal@nifty.com>
2022-09-21 16:44:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,24 @@ import cv2
 import numpy as np
 import requests
 
-# Title of the app
 st.title( 'Mobile Camera Preview in Streamlit' )
-
-# An empty image container that will hold the frames we'll fetch from the server
 frame_window = st.image( [] )
+take_picture_button = st.button( 'Take Picture' )
+
+while True:
+    # Request the image from the server
+    response = requests.get(url="http://<network_ip_address>:<port>/photo.jpg")
+    imgNp = np.array(bytearray(response.content), dtype=np.uint8)
+    frame = cv2.imdecode(imgNp, cv2.IMREAD_UNCHANGED )
+    # As OpenCV decodes images in BGR format, we'd convert it to the RGB format
+    frame = cv2.cvtColor( frame , cv2.COLOR_BGR2RGB )
+
+    frame_window.image(frame)
+
+    if take_picture_button:
+        # Pass the frame to a model
+        # And show the output here...
+        break
+
+
+
